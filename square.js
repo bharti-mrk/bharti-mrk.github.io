9a678e420ba394568a95fef08b4ce35d9c296bf2
bharti-mrk <108858403+bharti-mrk@users.noreply.github.com>
2022-07-07 13:28:49 +0530

    This is square exercise file.
    This is square exercise file.

---
+++ square.js
@@ -0,0 +1,7 @@
+function cal(){
+	var a = document.getElementById('text1').value;
+	var sarea = a*a;
+	document.getElementById('arsquare').value = sarea;
+	var speri = 4*a;
+	document.getElementById('peri').value = speri;
+}
